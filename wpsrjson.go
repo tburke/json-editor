740f7e8f6283a03d49d29572fd49f6b71a644884
Thomas Burke <tburke@wapolabs.com>
2012-04-02 12:35:16 -0400

    initial commit, combined web/cmd line editor.

---
+++ wpsrjson.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+    "encoding/json"
+    "net/http"
+    "log"
+    "os"
+    "io"
+    "strconv"
+    "fmt"
+    "html/template"
+)
+
+type Actor struct {
+    DisplayName string `json:"displayName"`
+    Url string `json:"url,omitempty"`
+    Rid int64 `json:"rid,omitempty"`
+    Id int64 `json:"id"`
+    ObjectType string `json:"objectType"`
+}
+
+func actors(filename string) (v map[string][]Actor) {
+    obj, _ := os.Open(filename)
+    dec := json.NewDecoder(obj)
+    if err := dec.Decode(&v); err != nil {
+        log.Println(err)
+        return
+    }
+    return v
+}
+
+func listActors(w io.Writer, actors []Actor) {
+    t, err := template.New("tib").Parse(`<h1>test</h1>
+{{range .}}{{.Id}} {{.DisplayName}}
+{{end}}`)
+    if err != nil {
+        fmt.Printf("err %v",err)
+        }
+    err = t.Execute(w,actors)
+    if err != nil {
+        fmt.Printf("err %v",err)
+        }
+}
+
+func editActor(w io.Writer, actors []Actor, id int64) {
+    t, _ := template.New("tib").Parse(`<h1>edit</h1>
+{{.Id}} {{.DisplayName}}
+`)
+    for _, a := range actors {
+        if a.Id == id {
+            t.Execute(w,a)
+        }
+    }
+}
+
+func edithandler(w http.ResponseWriter, r *http.Request) {
+    ID, _ := strconv.ParseInt(r.URL.Path[len("/edit/"):], 10,64)
+    v := actors("objects.json")
+    editActor(w, v["actors"], ID)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    v := actors("objects.json")
+    listActors(w, v["actors"])
+}
+
+func web() {
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/edit/", edithandler)
+    http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+    if len(os.Args) < 2 {
+        v := actors("objects.json")
+        listActors(os.Stdout, v["actors"])
+    } else if os.Args[1] == "web" {
+        web()
+    } else {
+        v := actors("objects.json")
+        ID, _ := strconv.ParseInt(os.Args[1],10,64)
+        editActor(os.Stdout, v["actors"],ID)
+    }
+}
+    /*
+    // dec := json.NewDecoder(os.Stdin)
+    enc := json.NewEncoder(os.Stdout)
+    for k, vv := range v {
+        if k == "actors" {
+            for _, u := range vv {
+                fmt.Printf("%d\t%v\n",u.Id, u)
+            }
+        }
+    }
+    if err := enc.Encode(&v); err != nil {
+        log.Println(err)
+    }
+    */
+
